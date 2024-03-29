471a6b3673f136fd9604b3ad0f582582b2044754
Arman Avetiysan <7474152+armleo@users.noreply.github.com>
2024-03-29 19:19:52 +0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,55 @@
+import tkinter as tk
+import tkinter.filedialog
+import os
+import random
+
+playlists_directories = []
+lbls = []
+playlists = dict()
+
+def newPlaylistSelect():
+    dir = tkinter.filedialog.askdirectory()
+    playlists_directories.append(dir)
+    
+    lbl = tk.Label(window,text=dir)
+    lbl.pack()
+    lbls.append(lbl)
+
+    print("Reading directory:", dir)
+    cont = os.listdir(dir)
+    print("Directory content:", len(cont), cont)
+    if len(cont) != 0:
+        random.shuffle(cont)
+        playlists[dir] = cont
+    
+
+    
+def saveNewPlaylist():
+    saveNewPlaylistPath = tkinter.filedialog.asksaveasfilename(filetypes=[("m3u result playlist","*.m3u")])
+    new_playlist = []
+    done = False
+    while not done:
+        for p in playlists_directories:
+            print("Working on ", p)
+            if (len(playlists[p]) != 0):
+                new_playlist.append(os.path.join(p,playlists[p].pop()))
+            else:
+                print("Done", len(playlists[p]))
+                done = True
+    
+    print(new_playlist)
+    print(saveNewPlaylistPath)
+    newPlaylistFile = open(saveNewPlaylistPath, "w+")
+    print("#EXTM3U",file=newPlaylistFile)
+    for f in new_playlist:
+        print(f, file=newPlaylistFile)
+    exit()
+
+window = tk.Tk()
+
+button = tk.Button(window, text='Select playlist folder', command=newPlaylistSelect)
+button.pack()
+save_button = tk.Button(window, text='Save playlist', command=saveNewPlaylist)
+save_button.pack()
+
+window.mainloop()
